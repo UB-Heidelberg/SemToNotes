cd88460131e72928d128df3d03c0b3abe2df66b5
JochenGraf <jochen.graf@uni-koeln.de>
2014-11-03 20:06:05 +0100

    optimizations for demo pages

--- client/src/widget/drawing/widgetCanvas.js
+++ client/src/widget/drawing/widgetCanvas.js
@@ -105,6 +105,7 @@ xrx.widget.Canvas.prototype.refresh = function() {
 
 
 xrx.widget.Canvas.prototype.createDom = function() {
+  console.log('###################################################');
   var self = this;
   // initialize drawing
   var datasetEngine = goog.dom.dataset.get(this.element_, 'xrxEngine');
