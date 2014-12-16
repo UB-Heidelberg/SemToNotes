195df1b80bcddb45bbf3906d4a9911844c7e7ce4
Jochen <jochen.graf@uni-koeln.de>
2014-12-16 09:20:19 +0100

    html5 forms implementation started

---
+++ client/src/widget/html5/html5Input.js
@@ -0,0 +1,52 @@
+***REMOVED***
+***REMOVED*** @fileoverview 
+***REMOVED***
+
+goog.provide('xrx.html5.Input');
+
+
+
+goog.require('goog.dom.forms');
+***REMOVED***
+***REMOVED***
+goog.require('xrx.mvc');
+goog.require('xrx.mvc.ComponentView');
+
+
+
+xrx.html5.Input = function(element) {
+
+***REMOVED***
+***REMOVED***
+goog.inherits(xrx.html5.Input, xrx.mvc.ComponentView);
+xrx.mvc.registerComponent('xrx-input', xrx.html5.Input);
+
+
+
+xrx.html5.Input.prototype.createDom = function() {
+***REMOVED***this.element_, goog.events.EventType.INPUT, function(e) {
+    e.preventDefault();
+    this.mvcModelUpdateData();
+  }, false, this);
+***REMOVED***
+
+
+
+xrx.html5.Input.prototype.mvcRefresh = function() {
+  var node = this.getNode();
+  var xml = node ? node.getXml() : undefined;
+  if (xml) goog.dom.forms.setValue(this.element_, xml);
+***REMOVED***
+
+
+
+xrx.html5.Input.prototype.mvcRemove = function() {
+  goog.dom.forms.setValue(this.element_, '');
+***REMOVED***
+
+
+
+xrx.html5.Input.prototype.mvcModelUpdateData = function() {
+  xrx.mvc.Controller.updateNode(this, this.getNode(),
+      goog.dom.forms.getValue(this.element_));
+***REMOVED***
---
+++ client/src/widget/html5/html5Textarea.js
@@ -0,0 +1,22 @@
+***REMOVED***
+***REMOVED*** @fileoverview 
+***REMOVED***
+
+goog.provide('xrx.html5.Textarea');
+
+
+
+goog.require('xrx.html5.Input');
+
+
+
+xrx.html5.Textarea = function(element) {
+
+***REMOVED***
+***REMOVED***
+goog.inherits(xrx.html5.Textarea, xrx.html5.Input);
+xrx.mvc.registerComponent('xrx-textarea', xrx.html5.Textarea);
+
+
+
+
